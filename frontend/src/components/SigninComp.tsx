387ecb3f17095eaf9d3be0d5cedfb4fd495a365d
Rathore <anukul.rathore@accenture.com>
2024-03-08 22:18:25 +0530

    third commit

--- frontend/src/components/SigninComp.tsx
+++ frontend/src/components/SigninComp.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { server_url } from '../config'
 import { SignupType } from '@anukulrthr/common'
